6d5ed4571676a343e0957b22eb79a6a55b873a1f
EC2 Default User <ec2-user@ip-172-31-43-144.ap-northeast-1.compute.internal>
2020-05-23 06:26:21 +0000

    blogsテーブルカラム追加

--- db/migrate/20200523061701_create_blogs.rb
+++ db/migrate/20200523061701_create_blogs.rb
@@ -1,7 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
-
+      t.string :nickname
+      t.text :text
+      t.integer :rate
+      
       t.timestamps
     end
   end
