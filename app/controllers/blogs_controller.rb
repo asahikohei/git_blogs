d238a39ce637e69208d94065be8164a243e90a42
EC2 Default User <ec2-user@ip-172-31-43-144.ap-northeast-1.compute.internal>
2020-05-25 05:38:53 +0000

    blogsコントローラーindex/show/new/createアクション設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,22 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all.order('created_at DESC').page(params[:page]).per(3)
   end
   
   def show
+    @blog = Blog.find(params[:id])
   end
   
   def new
+    @blog = Blog.new
   end
   
   def create
+    Blog.create(create_params)
+    redirect_to :root
+  end
+  
+  private
+  def create_params
   end
 end
